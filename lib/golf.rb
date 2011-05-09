b9d921cc2e08f418d9153281380820ec1e719fa8
Tom ten Thij <git@tomtenthij.nl>
2011-05-09 18:59:40 +0100

    hole 3 passes

--- lib/golf.rb
+++ lib/golf.rb
@@ -6,4 +6,12 @@ class Golf
   def self.hole2(s)
     s.split.sort_by { |w| w[1] }.join ' '
   end
+  
+  def self.hole3(n)
+    if n > 0
+      n * hole3(n - 1)
+    else
+      1
+    end
+  end
 end
